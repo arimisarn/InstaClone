2295db134474badefa99b11a3e22f4c942f8d0f3
arimisarn <arimisa.dev@gmail.com>
2025-08-01 08:54:40 +0300

    profile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "backend.urls"
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
